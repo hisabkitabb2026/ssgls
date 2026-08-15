4df5c70644b980a8ea1fa0239e20f7450c8c6470
hisabkitabb2026 <hisabkitabb2026@gmail.com>
2026-08-15 16:36:28 +0530

    Add SendDocumentMail, GenerateDocumentPdfJob, expense migration, and update BaseMultiselect and ConsolidationDetailView

--- Modules/HelloWorld/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\HelloWorld\Http\Controllers\HelloWorldController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('helloworlds', HelloWorldController::class)->names('helloworld');
-});
--- Modules/HelloWorld/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\HelloWorld\Http\Controllers\HelloWorldController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('helloworlds', HelloWorldController::class)->names('helloworld');
-});
